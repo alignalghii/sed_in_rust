24be79a1a828e559eedb443a8371eb6b82248901
Endrey Márk <ungazik@gmail.com>
2023-01-24 13:25:17 +0100

    A simple `cat` program tool: copy stdin to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let input_stream_channel = io::stdin();
+    loop {
+        let mut line = String::new();
+        if let Ok(n) = input_stream_channel.read_line(&mut line) {
+            if n > 0 {
+                trim_newline(&mut line);
+                println!("{line}");
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+// @credit to https://stackoverflow.com/q/37888042
+fn trim_newline(s: &mut String) {
+    if let Some(last) = s.pop() {
+        if last != '\n' {
+            s.push(last);
+        }
+    }
 }
